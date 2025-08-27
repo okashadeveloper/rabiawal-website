d19181e20958e86c745bbcaa62711bb3b2bb19bb
okashadeveloper <okashaahmedofficial@gmail.com>
2025-08-27 16:31:16 +0500

    home section

---
+++ script.js
@@ -0,0 +1,6 @@
+// Contact button functionality
+document.getElementById("contact").addEventListener("click", () => {
+  // yahan tum apna email link ya contact page ka link dal sakte ho
+  window.location.href = "contact.html"; 
+});
+
